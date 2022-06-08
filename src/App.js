548e03b411f2534828e8b36a79321aa9fe7639b5
junseolee <dlwnstjzld@gmail.com>
2022-06-08 10:24:46 +0900

    실습

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
-function Header() {
-  return (
-    <header>
-      <h1>
-        <a href='/'>Web</a>
-      </h1>
-    </header>
-  );
-}
-
-function Nav() {
-  return (
-    <nav>
-      <ol>
-        <li>
-          <a href='/read/1'>html</a>
-        </li>
-        <li>
-          <a href='/read/2'>css</a>
-        </li>
-      </ol>
-    </nav>
-  );
-}
-
-function Article() {
-  return (
-    <article>
-      <h2>Welcome</h2>
-      Hello, WEB!
-    </article>
-  );
-}
 function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
-    </div>
-  );
+  return <div>Hello, world!</div>;
 }
 
 export default App;
+
+// function Header() {
+//   return (
+//     <header>
+//       <h1>
+//         <a href='/'>Web</a>
+//       </h1>
+//     </header>
+//   );
+// }
+
+// function Nav(props) {
+//   const list = props.data.map((e) => {
+//     return (
+//       <li key={e.id}>
+//         <a href={'/read/' + e.id}>{e.title}</a>
+//       </li>
+//     );
+//   });
+//   return (
+//     <nav>
+//       <ol>{list}</ol>
+//     </nav>
+//   );
+// }
+
+// function Article(props) {
+//   return (
+//     <article>
+//       <h2>{props.title}</h2>
+//       {props.body}
+//     </article>
+//   );
+// }
+// function App() {
+//   const topics = [
+//     {id: 1, title: 'html', body: 'html is...'},
+//     {id: 2, title: 'css', body: 'css is...'},
+//   ];
+//   return (
+//     <div>
+//       <Header></Header>
+//       <Nav data={topics}></Nav>
+//       <Article title='wtf' body='this is react'></Article>
+//     </div>
+//   );
